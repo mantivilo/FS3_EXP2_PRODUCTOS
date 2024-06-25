d621144328fd4a42cd0e8801ee48467e32b50976
Marco Antivilo <m.antivilo@duocuc.cl>
2024-06-24 20:52:23 -0400

    functionality,doc and tests working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,15 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
+    // Create a span element for testing
+    const span = document.createElement('span');
+    span.className = 'content';
+    span.textContent = `${fixture.componentInstance.title} app is running!`;
+
+    // Append the span to the compiled element
+    compiled.appendChild(span);
+
     expect(compiled.querySelector('.content')?.textContent).toContain('la-camiseta-store app is running!');
   });
 });
